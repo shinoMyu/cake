20435f04367545d8b5ff731b4c5b7c95ece52ac4
myu <ymu75735@gmail.com>
2026-07-21 23:28:45 +0800

    Initialize React project

---
+++ react/src/pages/Home/index.js
@@ -0,0 +1,16 @@
+import Header from "./components/Header";
+import Introduction from "./components/Introduction";
+import Products from "./components/Products";
+import About from "./components/About";
+import Footer from "./components/Footer";
+
+const Home = () => {
+    return <div>
+        <Header />
+        <Introduction />
+        <Products />
+        <About />
+        <Footer />
+    </div>
+}
+export default Home;
